c5b56c1fe79720b307c9ca021c6260c2ffa88998
antonkostiuchkov <antonkostiuchkov@gmail.com>
2017-01-25 23:52:10 +0100

    custom form

--- src/app/tesla-battery/tesla-battery.module.ts
+++ src/app/tesla-battery/tesla-battery.module.ts
@@ -5,10 +5,20 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { BatteryService } from './tesla-battery.service';
 
 import { TeslaBatteryComponent } from './containers/tesla-battery/tesla-battery.component';
+import { TeslaCarComponent } from './components/tesla-car/tesla-car.component';
+import { TeslaStatsComponent } from './components/tesla-stats/tesla-stats.component';
+import { TeslaCounterComponent } from './components/tesla-counter/tesla-counter.component';
+import { TeslaClimateComponent } from './components/tesla-climate/tesla-climate.component';
+import { TeslaWheelsComponent } from './components/tesla-wheels/tesla-wheels.component';
 
 @NgModule({
   declarations: [
-    TeslaBatteryComponent
+    TeslaBatteryComponent,
+    TeslaCarComponent,
+    TeslaStatsComponent,
+    TeslaCounterComponent,
+    TeslaClimateComponent,
+    TeslaWheelsComponent
   ],
   imports: [
     CommonModule,
